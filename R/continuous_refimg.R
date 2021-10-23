7b2e92b46f7081dd548923d72f2e5be322adc058
pi514 <p.inglese@outlook.com>
2021-10-23 19:29:45 +0100

    fixed doc

--- R/continuous_refimg.R
+++ R/continuous_refimg.R
@@ -1,7 +1,7 @@
 
 #' \code{refImageContinuous} returns the reference image, calculated using the
-#' \code{refMethod}.
-#' This images represents the basic measure for the filters in SPUTNIK.
+#' \code{method}.
+#' This image represents the basic measure for the filters in SPUTNIK.
 #'
 #' @param msiData \link[SPUTNIK]{msiDataset} object..
 #' @param method string (default = "sum"). Method used to calculate the
@@ -12,35 +12,37 @@
 #'   \item "median": median peak intensities (without zeros)
 #'   \item "pca": first principal component scores.
 #' }
-#' @param mzQueryRef numeric. Values of m/z used to calculate the reference image.
-#' 2 values are interpreted as interval, multiple or single values are searched
-#' in the m/z vector. It overrides the param \code{useFullMZRef}.
-#' @param mzTolerance numeric (default = Inf). Tolerance in PPM to match the
-#' \code{mzQueryRef} values in the m/z vector.
-#' @param useFullMZRef logical (default = TRUE). Whether all the peaks should be
-#' used to calculate the reference image.
-#' @param doSmooth logical (default = FALSE). Whether the reference image
-#' should be smoothed before binarizing. Only valid for \code{roiMethod = "otsu"}.
-#' @param smoothSigma numeric (default = 2). Standard deviation of Gaussian
-#' kernel.
-#' @param alignTo string (default = "detected"). Image used as reference for
-#' aligning the estimated reference and ROI image. Valid values are:
+#' @param mzQueryRef numeric. Mass-to-charge ratios used to calculate the reference image.
+#' Two values are interpreted as interval, multiple or single values are searched
+#' in the m/z vector. It overrides the \code{useFullMZRef} argument.
+#' @param mzQueryTol numeric (default = Inf). Search window in parts-per-million units
+#' for the \code{mzQueryRef}.
+#' @param useFullMZRef logical (default = \code{TRUE}). Whether all peaks should be
+#' used to calculate the reference image. Ignored if \code{mzQueryRef} is provided.
+#' @param doSmooth logical (default = FALSE). If \code{TRUE}, the reference image is
+#' smoothed using a Gaussian kernel.
+#' @param smoothSigma numeric (default = 2). Standard deviation of the smoothing 
+#' Gaussian kernel.
+#' @param alignTo string (default = "detected"). The reference image is aligned
+#'to the image representing:
 #' \itemize{
 #'  \item "detected": number of detected peaks
 #'  \item "tic": total-ion-count image
 #' }
-#' @param invertAligned logical (default = FALSE). Whether the reference image
-#' colors should be positively or negatively correlated with the sample reference
-#' (defined by the argument \code{alignTo}). In case the sample reference is **higher**
-#' outside of the sample region, set it to TRUE.
+#' The reference image will have a positive Pearson's correlation with the selected image.
+#' @param invertAligned logical (default = FALSE). If \code{TRUE}, the reference image
+#' has negative correlation with the selected image in \code{alignTo}.
 #' @param verbose logical (default = TRUE). Additional output text.
 #'
 #' @details Function to extract the continuous reference image from a 
 #' \code{\link{msi.dataset-class}} object.
-#' Multiple methods can be used to extract the reference image, which afterwards 
-#' can be used as argument for the \code{\link{globalPeaksFilter}} filter.
-#'
-#' @author Paolo Inglese \email{p.inglese14@imperial.ac.uk}
+#' The continuous reference image represents the spatial location of the sample.
+#' By default, it is aligned with either the image representing the number of detected
+#' peaks, or the total-ion-count in all pixels. It is expected to be higher in the
+#' region occupied by the sample (positive correlation with the mask representing
+#' the real sample pixels). In some cases, the alignment images can have higher
+#' values in the pixels outside of the sample. In these circumstances, the argument
+#' \code{invertAligned} should be set to \code{TRUE}.
 #'
 #' @example R/examples/graph_funcs.R
 #'
