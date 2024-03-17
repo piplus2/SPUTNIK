a6470860419a7ac099e2c52aafe0060d6ae51592
paoloinglese <p.inglese@outlook.com>
2024-03-17 12:15:39 +0000

    update to 1.4.2

--- test_all.R
+++ test_all.R
@@ -9,7 +9,5 @@ source('tests/test_global_ref_filter.R')
 
 source('tests/test_pixel_count_filter.R')
 
-source('tests/test_csr_filter.R')
-
 test_results <- test_dir('tests/', reporter = 'summary')
 print(test_results)
